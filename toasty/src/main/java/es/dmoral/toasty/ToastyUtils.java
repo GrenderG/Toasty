6b9bdb02da1f1e4be4cbe8c53b4f7b58ec7f4060
Daniel Morales <dmorales@dmoral.es>
2017-12-11 15:33:59 +0100

    Update ToastyUtils.java

--- toasty/src/main/java/es/dmoral/toasty/ToastyUtils.java
+++ toasty/src/main/java/es/dmoral/toasty/ToastyUtils.java
@@ -14,7 +14,7 @@ import android.view.View;
  * This file is part of Toasty.
  * 
  * Toasty is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
