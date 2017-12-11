77807480a65aec4f08a08cc1ce89a6f61f1088f1
Daniel Morales <dmorales@dmoral.es>
2017-12-11 15:34:14 +0100

    Update Toasty.java

--- toasty/src/main/java/es/dmoral/toasty/Toasty.java
+++ toasty/src/main/java/es/dmoral/toasty/Toasty.java
@@ -20,7 +20,7 @@ import android.widget.Toast;
  * This file is part of Toasty.
  *
  * Toasty is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
